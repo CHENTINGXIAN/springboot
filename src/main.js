2e442d01257ede393b8db8e051477ff96469d855
chentingxian195467 <chentingxian195467@163.com>
2022-08-24 22:00:38 +0800

    the second edition

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/global.css'
+
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI,{size: "small"});   //可以不写{size: "small"}
+
 
 new Vue({
   router,
