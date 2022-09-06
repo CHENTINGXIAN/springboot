b000f918206ed4458bdc9e2139fb8b260381f3a9
chentingxian195467 <chentingxian195467@163.com>
2022-09-06 22:51:21 +0800

    尝试提交第三版代码

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -68,9 +68,7 @@
         </el-submenu>
       </el-menu>
     </el-aside>
-
     <el-container>
-
       <el-header style="font-size: 12px; border-bottom: 1px solid #ccc; line-height: 60px; display: flex">
 <!--        目录收缩展开的图标-->
         <div style="flex: 1; font-size: 20px">
@@ -83,9 +81,7 @@
             <el-dropdown-item>退出</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
-
       </el-header>
-
       <el-main>
         <div style="margin-bottom: 30px">
           <el-breadcrumb separator="/">
@@ -95,55 +91,44 @@
             <el-breadcrumb-item>用户管理</el-breadcrumb-item>
           </el-breadcrumb>
         </div>
-
-
-
-
         <div style="margin: 10px 0">
-          <el-input style="width: 200px" placeholder="请输入名称" suffix-icon="el-icon-search"></el-input>
-          <el-input style="width: 200px" placeholder="请输入邮箱" suffix-icon="el-icon-message" class="ml-5"></el-input>
-          <el-input style="width: 200px" placeholder="请输入地址" suffix-icon="el-icon-position" class="ml-5"></el-input>
-          <el-button class="ml-5" type="primary">搜索</el-button>
+          <el-input style="width: 200px" placeholder="请输入名称" suffix-icon="el-icon-search" v-model="username"></el-input>
+<!--          <el-input style="width: 200px" placeholder="请输入邮箱" suffix-icon="el-icon-message" class="ml-5"></el-input>-->
+<!--          <el-input style="width: 200px" placeholder="请输入地址" suffix-icon="el-icon-position" class="ml-5"></el-input>-->
+          <el-button class="ml-5" type="primary" @click="load">搜索</el-button>
         </div>
-
-
         <div style="margin: 10px 0">
           <el-button type="primary">新增<i class="el-icon-circle-plus-outline"></i></el-button>
           <el-button type="danger">批量删除<i class="el-icon-remove-outline"></i></el-button>
           <el-button type="primary">导入<i class="el-icon-bottom"></i></el-button>
           <el-button type="primary">导出<i class="el-icon-top"></i></el-button>
-
-
         </div>
 
-
-
-
         <el-table :data="tableData" border stripe :header-cell-class-name="headerBg">
-          <el-table-column prop="date" label="日期" width="140">
-          </el-table-column>
-          <el-table-column prop="name" label="姓名" width="120">
-          </el-table-column>
-          <el-table-column prop="address" label="地址">
-          </el-table-column>
-
-          <el-table-column label="操作">
+          <el-table-column prop="id" label="ID" width="80"></el-table-column>
+          <el-table-column prop="username" label="用户名" width="140"></el-table-column>
+          <el-table-column prop="nickname" label="昵称" width="120"></el-table-column>
+          <el-table-column prop="email" label="邮箱"></el-table-column>
+          <el-table-column prop="phone" label="电话"></el-table-column>
+          <el-table-column prop="address" label="地址"></el-table-column>
+          <el-table-column label="操作" width="200" align="center">
             <template slot-scope="scope">
               <el-button type="success">编辑 <i class="el-icon-edit"></i></el-button>
               <el-button type="danger">删除 <i class="el-icon-remove-outline"></i></el-button>
             </template>
           </el-table-column>
-
-
         </el-table>
-        <el-pagination
-            :page-sizes="[5, 10, 15, 20]"
-            :page-size="10"
-            layout="total, sizes, prev, pager, next, jumper"
-            :total="400">
-        </el-pagination>
-
-
+        <div style="padding: 10px 0">
+          <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page = "pageNum"
+              :page-sizes="[2, 5, 10, 15, 20]"
+              :page-size="pageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="total">
+          </el-pagination>
+        </div>
       </el-main>
 
     </el-container>
@@ -155,14 +140,19 @@
 export default {
   name: 'Home',
   data() {
-    const item = {
-      date: '2016-05-02',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
-    };
+    // const item = {
+    //   date: '2016-05-02',
+    //   name: '王小虎',
+    //   address: '上海市普陀区金沙江路 1518 弄'
+    // };
     return {
-      tableData: Array(10).fill(item),
-      msg: "hello 青哥哥",
+      // tableData: Array(10).fill(item),
+      tableData: [],
+      total: 0,
+      pageNum: 1,
+      pageSize: 2,
+      username: "",
+      msg: "hello,brother mine",
       collapseBtnClass: 'el-icon-s-fold',
       isCollapse: false,
       sideWidth: 200,
@@ -170,6 +160,12 @@ export default {
       headerBg:'headerBg'
     }
   },
+  created() {
+    this.load()
+  },
+
+
+
   methods: {
     collapse() {  // 点击收缩按钮触发
       this.isCollapse = !this.isCollapse
@@ -182,7 +178,27 @@ export default {
         this.collapseBtnClass = 'el-icon-s-fold'
         this.logoTextShow = true
       }
+    },
+    load(){
+      //请求分页查询
+      fetch("http://localhost:9090/user/page?pageNum=" + this.pageNum + "&pageSize=" + this.pageSize + "&username=" + this.username)
+          .then(res => res.json()).then(res => {
+        console.log(res)
+        this.tableData = res.data
+        this.total = res.total
+      })
+    },
+    handleSizeChange(pageSize){
+      console.log(pageSize)
+      this.pageSize = pageSize
+      this.load()
+    },
+    handleCurrentChange(pageNum){
+      console.log(pageNum)
+      this.pageNum = pageNum
+      this.load()
     }
+
   }
 }
 </script>
